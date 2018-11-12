d826dc1b985bd067e0c5be029955dc67b2668a86
Daniel Krasovski <44982407+Meap01@users.noreply.github.com>
2018-11-12 20:42:45 +0000

    updated identifier

--- Project1/Assets/scripts/script.js
+++ Project1/Assets/scripts/script.js
@@ -18,12 +18,12 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text 
   if (distance < 0) {3
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("timer").innerHTML = "EXPIRED";
   }
 }, 1000);
 
